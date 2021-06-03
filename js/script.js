f579693ab7fd9f7717874deb6bb73770faa1005c
Daniil Veselov <daniil.veselov2012@yandex.ru>
2021-06-03 19:25:30 +0300

    random

--- js/script.js
+++ js/script.js
@@ -1,38 +1,45 @@
 "use strict";
+
 let isNumber = function (n) {
-   return !isNaN(parseFloat(n)) && isFinite(n) && +n >= 1 && +n <= 100;
+   const a = !isNaN(parseFloat(n)) && isFinite(n) && +n >= 1 && +n <= 100;
+   if (!a) {
+      alert('Введи число!');
+   }
+   return a;
 };
 
-let y;
-
-do {
-   y = prompt('Загадывание случайного числа от 1 до 100');
-} while (!isNumber(y));
-
-let x;
-
-function chislo() {
-
-   do {
-      x = prompt('Угадай число от 1 до 100');
-   } while (!isNumber(x));
+function getRandomArbitrary(min = 1, max = 100) {
+   return Math.random() * (max - min) + min;
 }
 
-chislo();
+function start() {
+   let y = getRandomArbitrary();
+   y = Math.floor(y);
+   console.log(y);
 
-if (x > y) {
-   alert("Загаданное число меньше");
+   function chislo() {
+      let x;
+      do {
+         x = prompt('Угадай число от 1 до 100');
+         if (x === null) {
+            return alert('Игра окончена');
+         }
+      } while (!isNumber(x));
+
+      if (+x > y) {
+         alert("Загаданное число меньше");
+         chislo();
+      }
+      if (+x < y) {
+         alert("Загаданное число больше");
+         chislo();
+      }
+      if (+x === y) {
+         alert('Поздравляю, Вы угадали!!!');
+      }
+   }
    chislo();
-}
-if (x < y) {
-   alert("Загаданное число больше");
-   chislo();
+
 }
 
-
-
-
-
-
-
-
+start();
