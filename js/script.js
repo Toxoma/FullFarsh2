f16acac7fc4af86cecae1637a4256333dd01f74a
Daniil Veselov <daniil.veselov2012@yandex.ru>
2021-06-11 13:15:11 +0300

    усложнённое задание

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const wrap = document.querySelector('.wrapper');
+let newDiv;
 
 function DomElement(selector, height, width, bg, fontSize) {
    this.selector = selector;
@@ -8,10 +9,14 @@ function DomElement(selector, height, width, bg, fontSize) {
    this.width = width;
    this.bg = bg;
    this.fontSize = fontSize;
+   this.position = 'absolute';
+   this.top = 100;
+   this.left = 100;
 }
 
 DomElement.prototype.create = function () {
-   let newDiv;
+
+
    if (this.selector[0] === '.') {
       newDiv = document.createElement('div');
       newDiv.classList.add(this.selector.substr(1));
@@ -29,10 +34,47 @@ DomElement.prototype.create = function () {
    newDiv.style.height = this.height + 'px';
    newDiv.style.backgroundColor = this.bg;
    newDiv.style.fontSize = this.fontSize + 'px';
+   newDiv.style.position = this.position;
+   newDiv.style.top = this.top + 'px';
+   newDiv.style.left = this.left + 'px';
 
    newDiv.textContent = 'Привет мир!';
 
 };
 
-let first = new DomElement('.abc', 300, 500, 'red', 34);
-first.create();
+DomElement.prototype.event = function () {
+   document.addEventListener('keydown', (e) => {
+      switch (e.key) {
+         case 'ArrowUp':
+            this.top = this.top - 10;
+            newDiv.style.top = this.top + 'px';
+            break;
+         case 'ArrowDown':
+            this.top = this.top + 10;
+            newDiv.style.top = this.top + 'px';
+            break;
+         case 'ArrowLeft':
+            this.left = this.left - 10;
+            newDiv.style.left = this.left + 'px';
+            break;
+         case 'ArrowRight':
+            this.left = this.left + 10;
+            newDiv.style.left = this.left + 'px';
+            break;
+      }
+   });
+};
+
+let first = new DomElement('#abc', 100, 100, 'red', 16);
+
+document.addEventListener('DOMContentLoaded', () => {
+   first.create();
+   first.event();
+});
+
+
+
+
+
+
+
