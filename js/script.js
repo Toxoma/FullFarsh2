ea8241950fae72e20405875ccce15e2be762ff27
Daniil Veselov <daniil.veselov2012@yandex.ru>
2021-06-17 23:05:50 +0300

    lesson18

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@
 window.addEventListener('DOMContentLoaded', () => {
    'use strict';
 
+   //!timer
    function countTimer(deadline) {
       const timerHours = document.querySelector('#timer-hours'),
          timerMinutes = document.querySelector('#timer-minutes'),
@@ -53,6 +54,77 @@ window.addEventListener('DOMContentLoaded', () => {
    }
 
    countTimer('18 june 2021');
+
+
+   //!menu
+   
+   
+   const toggleMenu = () => {
+      const btnMenu = document.querySelector('.menu'),
+         menu = document.querySelector('menu'),
+         closeBtn = document.querySelector('.close-btn'),
+         menuItems = menu.querySelectorAll('ul>li>a');
+      
+        let count=-50,
+       intervalMenu;
+
+      let handlerAnimate = () => {
+        intervalMenu = requestAnimationFrame(handlerAnimate);
+         if (count < 0) {
+            count++;
+            menu.style.transform = `translate(${count*2}%)`;
+         } else {
+            cancelAnimationFrame(intervalMenu);
+         }
+      };
+
+      let closeMenu = () => {
+         menu.style.transform = `translate(-100%)`;
+         count = -50;
+      };
+
+      let event = () => {
+         if (menu.style.transform && menu.style.transform === `translate(0%)`) {
+            closeMenu();
+         } else {
+            intervalMenu = requestAnimationFrame(handlerAnimate);
+         }
+      };
+
+      btnMenu.addEventListener('click', event);
+
+      closeBtn.addEventListener('click', closeMenu);
+      menuItems.forEach(elem => elem.addEventListener('click', closeMenu));
+
+   };
+   
+   if (document.documentElement.clientWidth >= 768) {
+    toggleMenu();
+   }
+
+
+
+
+
+   //!popup
+   const togglePopUp = () => {
+      const popup = document.querySelector('.popup'),
+         popupBtns = document.querySelectorAll('.popup-btn'),
+         popupClose = document.querySelector('.popup-close');
+
+      popupBtns.forEach(btn => btn.addEventListener('click', () => {
+         popup.style.display = 'block';
+      }));
+
+      popupClose.addEventListener('click', () => {
+         popup.style.display = 'none';
+      });
+   };
+
+   togglePopUp();
+
+
+   
 });
 
 
