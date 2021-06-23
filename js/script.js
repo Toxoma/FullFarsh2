0f46587bb316dee2666deece768e40e8158bdd2d
Daniil Veselov <daniil.veselov2012@yandex.ru>
2021-06-23 15:16:40 +0300

    header

--- js/script.js
+++ js/script.js
@@ -422,38 +422,39 @@ window.addEventListener('DOMContentLoaded', () => {
 
    //!questions
    const form2 = document.getElementById('form2');
+      
 
-   form2.addEventListener('input', (e) => {
+   body.addEventListener('input', (e) => {
       let target = e.target;
 
-      if (target.matches('#form2-name,#form2-message')) {
+      if (target.matches('#form2-name,#form2-message,#form1-name')) {
          target.value = target.value.replace(/[^а-я\s\-]/i,'');
-      } else if (target.matches('#form2-email')) {
+      } else if (target.matches('#form2-email,#form1-email')) {
          target.value = target.value.replace(/[^a-z\@\-\_\.\!\~\*\']/gi, '');
          console.log(target.value);
-      } else if(target.matches('#form2-phone')) {
+      } else if(target.matches('#form2-phone,#form1-phone')) {
          target.value = target.value.replace(/[^\d\(\)\-]/i, '');
       }
    });
 
-   form2.addEventListener('focusout', (e) => {
+   body.addEventListener('focusout', (e) => {
       let target = e.target;
 
       if (target.value) {
          
-         if (target.matches('#form2-name,#form2-message')) {
+         if (target.matches('#form2-name,#form2-message,#form1-name')) {
             target.value = target.value.replace(/^\s+|\s+$/g, '');
             target.value = target.value.replace(/\s{2,}/g, ' ');
-         } else if (target.matches('#form2-email')) {
+         } else if (target.matches('#form2-email,#form1-email')) {
             target.value = target.value.replace(/^\-+|\-+$/g, '');
             target.value = target.value.replace(/\-{2,}/g, '-');
             console.log(target.value);
-         } else if (target.matches('#form2-phone')) {
+         } else if (target.matches('#form2-phone,#form1-phone')) {
             target.value = target.value.replace(/^\-+|\-+$/g, '');
             target.value = target.value.replace(/\-{2,}/g, '-');
          }
       
-         if (target.matches('#form2-name')) {
+         if (target.matches('#form2-name,#form1-name')) {
             let str = target.value;
             str = str.split(' ');
             str.forEach((el, id) => str[id] = el[0].toUpperCase() + el.substring(1).toLowerCase());
